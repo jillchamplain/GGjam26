8e628cc7e862d789e8c25086be634bea4af22cd2
jill <jillian.chapin@mymail.champlain.edu>
2026-01-31 11:14:22 -0500

    Scoring Logic

---
+++ GGJ26_Tennis/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using TMPro;
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] int p1Score;
+    [SerializeField] int p2Score;
+
+    [SerializeField] TextMeshProUGUI p1TF;
+    [SerializeField] TextMeshProUGUI p2TF;
+    [SerializeField] TextMeshProUGUI dueceTF;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreasePlayer1Score()
+    {
+		p1Score++;
+		HandleScores();
+	}
+
+    public void IncreasePlayer2Score()
+    {
+		p2Score++;
+		HandleScores();
+	}
+
+
+    void HandleScores()
+    {
+        p1TF.gameObject.SetActive(true);
+        p2TF.gameObject.SetActive(true);
+		dueceTF.gameObject.SetActive(false);
+
+		HandleScore(p1Score, p1TF);
+        HandleScore(p2Score, p2TF);
+
+
+		if (p1Score >= 4 && p1Score >= p2Score + 2)
+		{
+			p1TF.text = "Win";
+			//p2  Wins
+			return;
+		}
+
+		else if (p2Score >= 4 && p2Score >= p1Score + 2)
+        {
+            p2TF.text = "Win";
+            //p1 Wins
+            return;
+        }
+
+		if (p1Score == p2Score) //All
+		{
+			p2TF.text = "All";
+		}
+
+		if (p1Score >= 3 && p2Score >= 3) //Duece
+        {
+            if (p1Score >= p2Score + 1) //Adv
+            {
+                p1TF.text = "Adv";
+                return;
+            }
+            else if(p2Score >= p1Score + 1)
+            {
+                p2TF.text = "Adv";
+                return;
+            }
+
+            p1TF.gameObject.SetActive(false);
+            p2TF.gameObject.SetActive(false);
+            dueceTF.gameObject.SetActive(true);
+        }
+    }
+
+    void HandleScore(int score, TextMeshProUGUI tf)
+    {
+        switch(score)
+        {
+            case 0:
+                tf.text = "Love";
+                break;
+            case 1:
+                tf.text = "15";
+                break;
+            case 2:
+                tf.text = "30";
+                break;
+            default:
+                tf.text = "40";
+                break;
+        }
+    }
+
+    bool ScoreCheck(int score1, int score2)
+    {
+        return score1 > score2 + 2;
+    }
+}
